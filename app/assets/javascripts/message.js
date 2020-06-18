e38fe129f6edba6f68da87878a269b91ccc45122
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-06-18 12:50:50 +0900

    非同期（訂正）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -38,34 +38,31 @@ $(function(){
        </div>`
      return html;
    };
- }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    $('form')[0].reset();
-    $('.submit-btn').prop('disabled', false);
+  }
+  
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
-  .fail(function(){
-    alert('error');
-    $('.submit-btn').prop('disabled', false);
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+      $('.submit-btn').prop('disabled', false);
+    })
   })
-  // .always(function(){
-  //   $(".submit-btn").prop('disabled', false);
-  // })
-
-})
 });
 

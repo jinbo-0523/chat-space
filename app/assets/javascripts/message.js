1274fb766e7fe29a14dd2f2983b11e8e006c556d
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-06-18 16:28:00 +0900

    訂正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -57,11 +57,13 @@ $(function(){
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
-      $('.submit-btn').prop('disabled', false);
     })
     .fail(function(){
       alert('error');
-      $('.submit-btn').prop('disabled', false);
+     
+    })
+    .always(function(){
+       $('.submit-btn').prop('disabled', false);
     })
   })
 });

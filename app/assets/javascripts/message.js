0e21f1e7daf4a150962fa6839d2d0608820168d1
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-06-19 19:04:36 +0900

    Auto Update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,11 @@
 $(function(){ 
+  var last_message_id = $('.message:last').data("message-id");
+
   function buildHTML(message){
+
    if ( message.image ) {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id=${message.id}>
          <div class="message__upper-info">
            <div class="message__upper-info__talker">
              ${message.user_name}
@@ -19,9 +22,10 @@ $(function(){
          <img src=${message.image} >
        </div>`
      return html;
+
    } else {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id=${message.id}>
          <div class="message__upper-info">
            <div class="message__upper-info__talker">
              ${message.user_name}
@@ -43,15 +47,15 @@ $(function(){
   $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
@@ -66,5 +70,31 @@ $(function(){
        $('.submit-btn').prop('disabled', false);
     })
   })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
 
